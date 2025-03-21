c2f9f8bef02d57b45f0088140a8a3fbdb1b346ca
SirAmong <ariel.morocho@hotmail.com>
2025-03-21 11:13:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+// Animación de los girasoles como si el viento soplara
+    gsap.to(".sunflower", {
+      rotate: 3,
+      transformOrigin: "50% 50%",
+      duration: 4,
+      repeat: -1,
+      yoyo: true,
+      ease: "sine.inOut"
+    });
